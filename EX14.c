5fecd52a533209b658db3d98ffcae19dcafb3f75
atuiyish <tuyi95.at@gmail.com>
2023-10-03 19:47:30 +0200

    Chap 4

--- EX14.c
+++ EX14.c
@@ -6,14 +6,14 @@ int main()
     int i=1,a,b=0;
 
     system("clear");
-    printf("Entrez 7 nombres :\n");
+    printf("Entrez 8 nombres :\n");
 
     do
     {
         scanf("%d",&a);
         b=a+b;
         i++;
-    } while (i<=7);
+    } while (i<=8);
     system("clear");
     printf("%d\n",b);
     return 0;
