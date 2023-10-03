cbc625a0f49e4788c60cfdff9f5bc7a90a2072b8
atuiyish <tuyi95.at@gmail.com>
2023-10-03 20:01:57 +0200

    Chap 4

---
+++ EX15.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int i=1,a,b=0;
+
+    system("clear");
+    printf("Entrez 6 nombres :\n");
+
+    while (i<=6)
+    {
+        scanf("%d",&a);
+        b=a+b;
+        i++;
+    }
+    system("clear");
+    printf("%d\n",b);
+    return 0;
+}
