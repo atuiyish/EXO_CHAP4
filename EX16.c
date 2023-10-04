638856b8bf308085a351beef9474d7db8294cd32
atuiyish <tuyi95.at@gmail.com>
2023-10-04 17:29:09 +0200

    Chap 4

---
+++ EX16.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int i,a,b=0;
+
+    system("clear");
+    printf("Entrez 5 nombres :\n");
+
+    for (i=1;i<=5;i++)
+    {
+        scanf("%d",&a);
+        b=a+b;
+    }
+    system("clear");
+    printf("%d\n",b);
+    return 0;
+}
