cf3a1b15b2448a15b6b63a829066a11cb571e88b
atuiyish <tuyi95.at@gmail.com>
2023-10-11 18:25:33 +0200

    Chap 4

---
+++ EX38.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int a,b=0,i;
+
+    for (i = 1; i < 5; i++)
+    {
+        system("clear");
+        printf("Entrez 5 nombres entiers :\n");
+        scanf("%d",&a);
+        system("clear");
+        if (a == 50)
+        {
+            b = a;
+        }
+    }
+    if (b)
+    {
+        printf("Le nombre 50 était présent dans la séquence\n");
+        return 0;
+    }
+    else
+    printf("Le nombre 50 n'était pas présent dans la séquence\n");
+    return 0;
+}
