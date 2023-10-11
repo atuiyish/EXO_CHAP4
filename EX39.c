a3454dcb3cf9644d81a5d97b0c6da3d7c12d73fa
atuiyish <tuyi95.at@gmail.com>
2023-10-11 18:30:55 +0200

    Chap 4

---
+++ EX39.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int a,b=0,i=1;
+
+    while (i < 7)
+    {
+        system("clear");
+        printf("Entrez 7 nombres entiers :\n");
+        scanf("%d",&a);
+        system("clear");
+
+        if (a == 34)
+        {
+            b = a;
+        }
+        i++;
+    }
+    if (b)
+    {
+        printf("Le nombre 34 était présent dans la séquence\n");
+        return 0;
+    }
+    else
+    printf("Le nombre 34 n'était pas présent dans la séquence\n");
+    return 0;
+}
