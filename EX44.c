753a8738495177598793f6cc23e0eba1102bac72
atuiyish <tuyi95.at@gmail.com>
2023-10-11 19:29:00 +0200

    Chap 4

---
+++ EX44.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int i=0,nbr;
+
+    system("clear");
+    printf("Entrez une cote entre 0 et 10 :\n");
+    scanf("%d",&nbr);
+    system("clear");
+
+    while (nbr < 0 || nbr > 10)
+    {
+        printf("ERROR !\n");
+        printf("Entrez une cote entre 0 et 10 :\n");
+        scanf("%d",&nbr);
+        system("clear");
+    }
+    return 0;
+}
