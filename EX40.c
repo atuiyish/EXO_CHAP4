11e2edc3080f6736dbeb9dd915a0fea6a7f452bd
atuiyish <tuyi95.at@gmail.com>
2023-10-11 18:34:14 +0200

    Chap 4

---
+++ EX40.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int a,b=0,i=1;
+
+    do
+    {
+        system("clear");
+        printf("Entrez 10 nombres entiers :\n");
+        scanf("%d",&a);
+        system("clear");
+
+        if (a == 8)
+        {
+            b = a;
+        }
+        i++;
+    } while (i < 10);
+    
+    if (b)
+    {
+        printf("Le nombre 8 était présent dans la séquence\n");
+        return 0;
+    }
+    else
+    printf("Le nombre 8 n'était pas présent dans la séquence\n");
+    return 0;
+}
