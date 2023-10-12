36e6ccfad92a16729b197f8e73632a0dc4ad6e09
atuiyish <tuyi95.at@gmail.com>
2023-10-12 22:01:41 +0200

    Chap 4

---
+++ EX46.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int i,nbr,a,b;
+
+    system("clear");
+    for (i = 0, a = 0; i < 5 && nbr >= 0 && nbr <= 20; i++)
+    {
+        printf("Entrez une cote entre 0 et 20 :\n");
+        scanf("%d",&nbr);
+        system("clear");
+        a = nbr + a;
+
+        for (b = a - nbr;nbr < 0 || nbr > 20;)
+        {
+            printf("ERROR !\n");
+            printf("Entrez une cote entre 0 et 20 :\n");
+            scanf("%d",&nbr);
+            
+            if (nbr > 0 && nbr < 20)
+            {
+                system("clear");
+                a = nbr + b;
+                printf("%d correct\n\v",nbr);
+            } 
+        }
+    }
+    printf("La moyenne est de %d%%\n",a);
+    return 0;
+}
